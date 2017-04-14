476f02aeb3ec4d642c108299a390c6f56c2a1a63
mateusz <mateuszbak3@gmail.com>
2017-04-14 17:36:54 +0200

    Add bigger screens support

--- app/src/main/java/bak/mateusz/quiz/QuizDetailActivity.java
+++ app/src/main/java/bak/mateusz/quiz/QuizDetailActivity.java
@@ -6,7 +6,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -57,18 +56,7 @@ public class QuizDetailActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
         if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
             Bundle arguments = new Bundle();
             arguments.putLong(QuizDetailFragment.ARG_ITEM_ID,
                     getIntent().getLongExtra(QuizDetailFragment.ARG_ITEM_ID, 0));
@@ -78,22 +66,6 @@ public class QuizDetailActivity extends AppCompatActivity {
                     .add(R.id.quiz_detail_container, fragment)
                     .commit();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            navigateUpTo(new Intent(this, QuizListActivity.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -115,6 +87,5 @@ public class QuizDetailActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         EventBus.getDefault().register(this);
-
     }
 }
--- app/src/main/java/bak/mateusz/quiz/QuizDetailFragment.java
+++ app/src/main/java/bak/mateusz/quiz/QuizDetailFragment.java
@@ -1,16 +1,28 @@
 package bak.mateusz.quiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import bak.mateusz.quiz.models.Item;
 import bak.mateusz.quiz.models.quiz.Quiz;
+import bak.mateusz.quiz.network.NetworkCalls;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -33,10 +45,18 @@ public class QuizDetailFragment extends Fragment {
     TextView message;
     @BindView(R.id.result)
     TextView result;
+    @Nullable
+    @BindView(R.id.quiz_image)
+    ImageView quizImage;
+    @Nullable
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
     private Item mItem;
     private Quiz quiz;
     private Unbinder bind;
     private CollapsingToolbarLayout appBarLayout;
+    private boolean mTwoPane = false;
+    private FrameLayout fragmentContainer;
 
     public QuizDetailFragment() {
     }
@@ -49,23 +69,44 @@ public class QuizDetailFragment extends Fragment {
             Activity activity = this.getActivity();
             appBarLayout =
                     (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-
+            fragmentContainer = (FrameLayout) activity.findViewById(R.id.quiz_detail_container);
+        }
+        if (getActivity().findViewById(R.id.twoPaneLayout) != null) {
+            mTwoPane = true;
         }
     }
 
 
     @Override
+    public void onPause() {
+        EventBus.getDefault().unregister(this);
+        super.onPause();
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
+        EventBus.getDefault().register(this);
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             Realm realm = Realm.getDefaultInstance();
             mItem = realm.where(Item.class).equalTo("id", getArguments().getLong(ARG_ITEM_ID)).findFirst();
             quiz = realm.where(Quiz.class).equalTo("id", getArguments().getLong(ARG_ITEM_ID)).findFirst();
         }
-        if (appBarLayout != null) {
+        if (!mTwoPane) {
             appBarLayout.setTitle(mItem.getCategory().getName().toUpperCase());
         } else {
+            assert fab != null;
+            fab.show();
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    NetworkCalls.getQuiz(getArguments().getLong(QuizDetailFragment.ARG_ITEM_ID));
+                }
+            });
             message.setText(mItem.getContent());
+            String picUrl = getArguments().getString(QuizDetailFragment.ARG_ITEM_PIC_URL);
+            Glide.with(this).load(picUrl).centerCrop().crossFade()
+                    .into(quizImage);
         }
         if (quiz != null) {
             if (quiz.getCurrentQuestion() == 0) {
@@ -97,5 +138,15 @@ public class QuizDetailFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         bind.unbind();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onQuizReceived(Quiz quiz) {
+        if (mTwoPane) {
+            Intent intent = new Intent(this.getActivity(), QuizActivity.class);
+            intent.putExtra(QuizDetailFragment.ARG_ITEM_ID,
+                    getArguments().getLong(QuizDetailFragment.ARG_ITEM_ID, 0));
+            startActivity(intent);
+        }
     }
 }
